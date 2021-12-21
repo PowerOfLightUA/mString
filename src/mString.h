83225ebb1401b219bdcce05d381605fd015c4c2f
Timofey <Timofey45.16.95@gmail.com>
2021-12-21 10:37:31 +0200

    update for c2000 controllers

--- src/mString.h
+++ src/mString.h
@@ -1,335 +1,362 @@
 /*
-    РЕЗКАЯ КАК ПОНОС И ЛЁГКАЯ КАК ПЁРЫШКО ЗАМЕНА STRING
-    Документация:
-    GitHub: https://github.com/GyverLibs/mString
-    Возможности:
-    - Практически полный аналог String, но статический
-    - Есть несколько встроенных функций парсинга строк
-    
-    AlexGyver, alex@alexgyver.ru
-    https://alexgyver.ru/
-    MIT License
+ РЕЗКАЯ КАК ПОНОС И ЛЁГКАЯ КАК ПЁРЫШКО ЗАМЕНА STRING
+ Документация:
+ GitHub: https://github.com/GyverLibs/mString
+ Возможности:
+ - Практически полный аналог String, но статический
+ - Есть несколько встроенных функций парсинга строк
 
-    Версии:
-    v1.0 - релиз
-*/
+ AlexGyver, alex@alexgyver.ru
+ https://alexgyver.ru/
+ MIT License
+
+ Версии:
+ v1.0 - релиз
+ */
 
 #ifndef mString_h
 #define mString_h
 
-#include <Arduino.h>
+#include <string>
+#include <string.h>
+using namespace std;
+
+#define DEC 10
+#define HEX 16
 
 // закодил, но тут вроде не используется
-char* mUtoa(uint32_t value, char *buffer, bool clear = 1);
-char* mLtoa(int32_t value, char *buffer, bool clear = 1);
-char* mFtoa(double value, int8_t decimals, char *buffer);
+char* mUtoa(Uint32 value, char *buffer, bool clear = 1);
+char* mLtoa(int32 value, char *buffer, bool clear = 1);
+char* mFtoa(double value, char decimals, char *buffer);
 
-char* mUtoa(uint32_t value, char *buffer, bool clear) {
+char* mUtoa(Uint32 value, char *buffer, bool clear)
+{
     buffer += 11;
-    if (clear) *--buffer = 0;
-    do {
+    if (clear)
+        *--buffer = 0;
+    do
+    {
         *--buffer = value % 10 + '0';
         value /= 10;
-    } while (value != 0);
+    }
+    while (value != 0);
     return buffer;
 }
 
-char* mLtoa(int32_t value, char *buffer, bool clear) {
+char* mLtoa(int32 value, char *buffer, bool clear)
+{
     bool minus = value < 0;
-    if (minus) value = -value;
+    if (minus)
+        value = -value;
     buffer = mUtoa(value, buffer, clear);
-    if (minus) *--buffer = '-';
+    if (minus)
+        *--buffer = '-';
     return buffer;
 }
 
-char* mFtoa(double value, int8_t decimals, char *buffer) {
-    int32_t mant = (int32_t)value;
+char* mFtoa(double value, Uint16 decimals, char *buffer)
+{
+    int32 mant = (int32) value;
     value -= mant;
-    uint32_t exp = 1;
-    while (decimals--) exp *= 10;
-    exp *= (float)value;
+    Uint32 exp = 1;
+    while (decimals--)
+        exp *= 10;
+    exp *= (float) value;
     buffer = ltoa(mant, buffer, DEC);
-    byte len = strlen(buffer);
+    char len = strlen(buffer);
     *(buffer + len++) = '.';
     ltoa(exp, buffer + len++, DEC);
     return buffer;
 }
 
-template < uint16_t _MS_SIZE >
-class mString {
+template<Uint16 _MS_SIZE>
+class mString
+{
 public:
     char buf[_MS_SIZE];
-    
-    mString() {
+
+    mString()
+    {
         clear();
     }
-    
-    uint16_t length() {
+
+    Uint16 length()
+    {
         return strlen(buf);
     }
-    
-    void clear() {
+
+    void clear()
+    {
         buf[0] = '\0';
     }
 
     // add
-    mString& add(const char c) {
+    mString& add(const char c)
+    {
         int len = length();
-        if (len + 1 >= _MS_SIZE) return *this;
+        if (len + 1 >= _MS_SIZE)
+            return *this;
         buf[len++] = c;
         buf[len] = '\0';
         return *this;
     }
-    mString& add(const char* data) {
-        if (length() + strlen(data) >= _MS_SIZE) return *this;
+    mString& add(const char *data)
+    {
+        if (length() + strlen(data) >= _MS_SIZE)
+            return *this;
         strcat(buf, data);
         return *this;
     }
-    mString& add(const __FlashStringHelper *data) {
-        PGM_P p = reinterpret_cast<PGM_P>(data);
-        if (length() + strlen_P(p) >= _MS_SIZE) return *this;
-        strcpy_P(buf + length(), p);
-        return *this;
-    }
-    mString& add(uint32_t value) {
+    mString& add(Uint32 value)
+    {
         char vBuf[11];
         utoa(value, vBuf, DEC);
         return add(vBuf);
     }
-    mString& add(uint16_t value) {
-        return add((uint32_t)value);
+    mString& add(Uint16 value)
+    {
+        return add((Uint32) value);
     }
-    mString& add(uint8_t value) {
-        return add((uint32_t)value);
-    }
-    mString& add(int32_t value) {
+    mString& add(int32 value)
+    {
         char vBuf[11];
         ltoa(value, vBuf, DEC);
         return add(vBuf);
     }
-    mString& add(int16_t value) {
-        return add((int32_t)value);
+    mString& add(int16 value)
+    {
+        return add((int32) value);
     }
-    mString& add(int8_t value) {
-        return add((int32_t)value);
-    }
-    mString& add(double value, int8_t dec = 2) {
+    mString& add(double value, Uint16 dec = 2)
+    {
         char vBuf[20];
         mFtoa(value, dec, vBuf);
         return add(vBuf);
     }
-    mString& add(mString data) {
+    mString& add(mString data)
+    {
         return add(data.buf);
     }
-    mString& add(String data) {
+    mString& add(string data)
+    {
         return add(data.c_str());
     }
 
     // add +=
-    mString& operator += (const char c) {
+    mString& operator +=(const char c)
+    {
         return add(c);
     }
-    mString& operator += (const char* data) {
+    mString& operator +=(const char *data)
+    {
         return add(data);
     }
-    mString& operator += (const __FlashStringHelper *data) {
-        return add(data);
-    }
-    mString& operator += (uint32_t value) {
+    mString& operator +=(Uint32 value)
+    {
         return add(value);
     }
-    mString& operator += (int32_t value) {
+    mString& operator +=(int32 value)
+    {
         return add(value);
     }
-    mString& operator += (uint16_t value) {
+    mString& operator +=(Uint16 value)
+    {
         return add(value);
     }
-    mString& operator += (int16_t value) {
+    mString& operator +=(int16 value)
+    {
         return add(value);
     }
-    mString& operator += (uint8_t value) {
+    mString& operator +=(double value)
+    {
         return add(value);
     }
-    mString& operator += (int8_t value) {
-        return add(value);
-    }
-    mString& operator += (double value) {
-        return add(value);
-    }
-    mString& operator += (mString data) {
+    mString& operator +=(mString data)
+    {
         return add(data.buf);
     }
-    mString& operator += (String data) {
+    mString& operator +=(string data)
+    {
         return add(data);
     }
 
     // +
-    mString operator + (const char c) {
+    mString operator +(const char c)
+    {
         return (*this).add(c);
     }
-    mString operator + (const char* data) {
+    mString operator +(const char *data)
+    {
         return (*this).add(data);
     }
-    mString operator + (const __FlashStringHelper *data) {
+    mString operator +(Uint32 value)
+    {
+        return (*this).add(value);
+    }
+    mString operator +(int32 value)
+    {
+        return (*this).add(value);
+    }
+    mString operator +(Uint16 value)
+    {
+        return (*this).add(value);
+    }
+    mString operator +(int16 value)
+    {
+        return (*this).add(value);
+    }
+    mString operator +(double value)
+    {
+        return (*this).add(value);
+    }
+    mString operator +(mString data)
+    {
         return (*this).add(data);
     }
-    mString operator + (uint32_t value) {
-        return (*this).add(value);
-    }
-    mString operator + (int32_t value) {
-        return (*this).add(value);
-    }
-    mString operator + (uint16_t value) {
-        return (*this).add(value);
-    }
-    mString operator + (int16_t value) {
-        return (*this).add(value);
-    }
-    mString operator + (uint8_t value) {
-        return (*this).add(value);
-    }
-    mString operator + (int8_t value) {
-        return (*this).add(value);
-    }
-    mString operator + (double value) {
-        return (*this).add(value);
-    }
-    mString operator + (mString data) {
-        return (*this).add(data);
-    }
-    mString operator + (String data) {
+    mString operator +(string data)
+    {
         return (*this).add(data);
     }
 
     // assign
-    mString& operator = (const char c) {
+    mString& operator =(const char c)
+    {
         clear();
         return add(c);
     }
-    mString& operator = (const char* data) {
+    mString& operator =(const char *data)
+    {
         clear();
         return add(data);
     }
-    mString& operator = (const __FlashStringHelper *data) {
-        clear();
-        return add(data);
-    }
-    mString& operator = (uint32_t value) {
+    mString& operator =(Uint32 value)
+    {
         clear();
         return add(value);
     }
-    mString& operator = (int32_t value) {
+    mString& operator =(int32 value)
+    {
         clear();
         return add(value);
     }
-    mString& operator = (uint16_t value) {
+    mString& operator =(Uint16 value)
+    {
         clear();
         return add(value);
     }
-    mString& operator = (int16_t value) {
+    mString& operator =(int16 value)
+    {
         clear();
         return add(value);
     }
-    mString& operator = (uint8_t value) {
+    mString& operator =(double value)
+    {
         clear();
         return add(value);
     }
-    mString& operator = (int8_t value) {
-        clear();
-        return add(value);
-    }
-    mString& operator = (double value) {
-        clear();
-        return add(value);
-    }
-    mString& operator = (String data) {
+    mString& operator =(string data)
+    {
         clear();
         return add(data);
     }
 
     // compare
-    bool operator == (const char c) {
+    bool operator ==(const char c)
+    {
         return (buf[0] == c && buf[1] == 0);
     }
-    bool operator == (const char* data) {
+    bool operator ==(const char *data)
+    {
         return !strcmp(buf, data);
     }
-    bool operator == (uint32_t value) {
+    bool operator ==(Uint32 value)
+    {
         char valBuf[11];
         return !strcmp(buf, utoa(value, valBuf, DEC));
     }
-    bool operator == (uint16_t value) {
+    bool operator ==(Uint16 value)
+    {
         char valBuf[6];
         return !strcmp(buf, utoa(value, valBuf, DEC));
     }
-    bool operator == (uint8_t value) {
-        char valBuf[4];
-        return !strcmp(buf, utoa(value, valBuf, DEC));
-    }
-    bool operator == (int32_t value) {
+    bool operator ==(int32 value)
+    {
         char valBuf[11];
         return !strcmp(buf, ltoa(value, valBuf, DEC));
     }
-    bool operator == (int16_t value) {
+    bool operator ==(int16 value)
+    {
         char valBuf[6];
         return !strcmp(buf, ltoa(value, valBuf, DEC));
     }
-    bool operator == (int8_t value) {
-        char valBuf[4];
-        return !strcmp(buf, ltoa(value, valBuf, DEC));
-    }
-    bool operator == (float value) {
+    bool operator ==(float value)
+    {
         char valBuf[20];
         return !strcmp(buf, mFtoa(value, 2, valBuf));
     }
-    bool operator == (mString data) {
+    bool operator ==(mString data)
+    {
         return !strcmp(buf, data.buf);
     }
-    bool operator == (String data) {
+    bool operator ==(string data)
+    {
         return !strcmp(buf, data.c_str());
     }
 
     // convert & parse
-    char operator [] (uint16_t index) const {
+    char operator [](Uint16 index) const
+    {
         return buf[index];
     }
-    char& operator [] (uint16_t index) {
+    char& operator [](Uint16 index)
+    {
         return buf[index];
     }
-    char charAt(uint16_t index) {
+    char charAt(Uint16 index)
+    {
         return buf[index];
     }
-    void setCharAt(uint16_t index, char c) {
+    void setCharAt(Uint16 index, char c)
+    {
         buf[index] = c;
     }
-    int32_t toInt(uint16_t from = 0) {
+    int32 toInt(Uint16 from = 0)
+    {
         return atol(buf + from);
     }
-    uint32_t toUint(uint16_t from = 0) {
+    Uint32 toUint(Uint16 from = 0)
+    {
         return atol(buf + from);
     }
-    float toFloat(uint16_t from = 0) {
+    float toFloat(Uint16 from = 0)
+    {
         return atof(buf + from);
     }
-    const char* c_str() {
+    const char* c_str()
+    {
         return buf;
     }
 
-    bool startsWith(const char *data, uint16_t offset = 0) {
+    bool startsWith(const char *data, Uint16 offset = 0)
+    {
         return strlen(data) == strspn(buf + offset, data);
     }
 
-    void substring(uint16_t from, uint16_t to, char* arr) {
+    void substring(Uint16 from, Uint16 to, char *arr)
+    {
         char backup = buf[++to];
         buf[to] = '\0';
         strcpy(arr, buf + from);
         buf[to] = backup;
     }
-    int split(char** ptrs, char div = ',') {
+    int split(char **ptrs, char div = ',')
+    {
         int i = 0, j = 1;
         ptrs[0] = buf;
-        while (buf[i]) {
-            if (buf[i] == div) {
+        while (buf[i])
+        {
+            if (buf[i] == div)
+            {
                 buf[i] = '\0';
                 ptrs[j++] = buf + i + 1;
             }
@@ -337,16 +364,23 @@ public:
         }
         return j;
     }
-    void truncate(uint16_t amount) {
-        uint16_t len = length();
-        if (amount >= len) clear();
-        else buf[len - amount] = '\0';
+    void truncate(Uint16 amount)
+    {
+        Uint16 len = length();
+        if (amount >= len)
+            clear();
+        else
+            buf[len - amount] = '\0';
     }
-    void remove(uint16_t index, uint16_t count) {
-        uint16_t len = length();
-        if (index >= len) return;
-        if (count <= 0) return;
-        if (count > len - index) {
+    void remove(Uint16 index, Uint16 count)
+    {
+        Uint16 len = length();
+        if (index >= len)
+            return;
+        if (count <= 0)
+            return;
+        if (count > len - index)
+        {
             count = len - index;
         }
         char *writeTo = buf + index;
@@ -355,96 +389,117 @@ public:
         buf[len] = 0;
     }
 
-    void toLowerCase() {
-        if (!length()) return;
-        for (char *p = buf; *p; p++) *p = tolower(*p);
+    void toLowerCase()
+    {
+        if (!length())
+            return;
+        for (char *p = buf; *p; p++)
+            *p = tolower(*p);
     }
 
-    void toUpperCase() {
-        if (!length()) return;
-        for (char *p = buf; *p; p++) *p = toupper(*p);
+    void toUpperCase()
+    {
+        if (!length())
+            return;
+        for (char *p = buf; *p; p++)
+            *p = toupper(*p);
     }
 
-    int indexOf(char ch, uint16_t fromIndex = 0) {
-        if (fromIndex >= length()) return -1;
-        const char* temp = strchr(buf + fromIndex, ch);
+    int indexOf(char ch, Uint16 fromIndex = 0)
+    {
+        if (fromIndex >= length())
+            return -1;
+        const char *temp = strchr(buf + fromIndex, ch);
         return (temp == NULL) ? -1 : (temp - buf);
     }
 
-    int indexOf(char* ch, uint16_t fromIndex = 0) {
-        if (fromIndex >= length()) return -1;
-        const char* temp = strstr(buf + fromIndex, ch);
+    int indexOf(char *ch, Uint16 fromIndex = 0)
+    {
+        if (fromIndex >= length())
+            return -1;
+        const char *temp = strstr(buf + fromIndex, ch);
         return (temp == NULL) ? -1 : (temp - buf);
     }
 
-    int parseBytes(byte* data, int len, char div = ',', char ter = '\0') {
+    int parseBytes(char *data, int len, char div = ',', char ter = '\0')
+    {
         int b = 0, c = 0;
         data[b] = 0;
-        while (true) {
-            if (buf[c] == div) {
+        while (true)
+        {
+            if (buf[c] == div)
+            {
                 b++;
                 c++;
-                if (b == len) return b;
+                if (b == len)
+                    return b;
                 data[b] = 0;
                 continue;
             }
-            if (buf[c] == ter || b == len) return b + 1;
+            if (buf[c] == ter || b == len)
+                return b + 1;
             data[b] *= 10;
             data[b] += buf[c] - '0';
             c++;
         }
     }
-    int parseInts(int* data, int len, char div = ',', char ter = '\0') {
+    int parseInts(int *data, int len, char div = ',', char ter = '\0')
+    {
         int b = 0, c = 0;
         data[b] = 0;
-        while (true) {
-            if (buf[c] == div) {
+        while (true)
+        {
+            if (buf[c] == div)
+            {
                 b++;
                 c++;
-                if (b == len) return b;
+                if (b == len)
+                    return b;
                 data[b] = 0;
                 continue;
             }
-            if (buf[c] == ter || b == len) return b + 1;
+            if (buf[c] == ter || b == len)
+                return b + 1;
             data[b] *= 10;
             data[b] += buf[c] - '0';
             c++;
         }
     }
     //cast operators
-    operator const char*(){ // теперь mString автоматом переконвертируется в cstring (const char *) если будет нужно
+    operator const char*()
+    { // теперь mString автоматом переконвертируется в cstring (const char *) если будет нужно
         return this->c_str();
     }
     /*
-    Пример:
-    void printString(const char * cstr){cout<<cstr;}
-    mString<16> mStr = "Hello";
-    
-        Было:
-        printString(mStr.c_str());
-        Стало:
-        printString(mStr);
-    
-    */
-    
+     Пример:
+     void printString(const char * cstr){cout<<cstr;}
+     mString<16> mStr = "Hello";
+
+     Было:
+     printString(mStr.c_str());
+     Стало:
+     printString(mStr);
+
+     */
+
     operator bool()
     {
         return (this->length());
     }
-    /* Теперь строка может возвращать bool 
-    Пример:
-        Было:
-        if(str.length())
-        {
-            doSomeMagic();
-        }
-        
-        Стало:
-        if(str)
-        {
-            doSomeMagic();
-        }
-    */
+    /* Теперь строка может возвращать bool
+     Пример:
+     Было:
+     if(str.length())
+     {
+     doSomeMagic();
+     }
+
+     Стало:
+     if(str)
+     {
+     doSomeMagic();
+     }
+     */
 private:
 };
 #endif
